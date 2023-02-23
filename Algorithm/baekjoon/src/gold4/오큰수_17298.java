822883bd782862fb1eb9b8ff8ca74bf02a1e3d90
DHKIM-0511 <122420197+DHKIM-0511@users.noreply.github.com>
2023-02-24 01:03:47 +0900

    0223 study-1

--- Algorithm/baekjoon/src/gold4/오큰수_17298.java
+++ Algorithm/baekjoon/src/gold4/오큰수_17298.java
@@ -1,5 +1,41 @@
 package gold4;
 
-public class 오큰수_17298 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
+public class 오큰수_17298 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] NGE = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Stack<Integer> tmp = new Stack<>();
+		
+		//비교 인덱스
+		int start = 0;
+		tmp.push(Integer.parseInt(st.nextToken())); // 5 7
+		while(true) {
+			int next;
+			if(st.hasMoreTokens()) {
+				 next = Integer.parseInt(st.nextToken()); // 5 2 7
+			}else if(tmp.size()!=1)
+				next = tmp.pop();
+			
+			if(next > tmp.peek()) { 
+				NGE[start++] = next;  // 5 7
+				tmp.pop();
+				tmp.push(next);
+				
+			}else
+				tmp.push(next);
+		}
+		//NGE(i+1) ~ NGE(N) 을 공백띄고 출력
+		
+		sb.append(-1);
+		System.out.println(sb); // NGE(i) = i 번째 인덱스보다 오른쪽이면서 큰 수.
+	}
 }
---
+++ Algorithm/swea/src/d3/암호생성기_1225.java
@@ -0,0 +1,46 @@
+package d3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 암호생성기_1225 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] pw = new int[8];
+		int front = 0;
+		
+		for(int i = 0 ; i < 8 ; i++)
+			pw[i] = Integer.parseInt(st.nextToken());
+		
+		out:while(true) {
+			
+			for(int i = 1 ; i <= 5 ; i++) {
+				
+				pw[front] = pw[front] - i;
+				
+				if(pw[front] <= 0) {
+					pw[front] = 0;
+					front = (front+1) % 8;
+					break out;
+				}else
+					front = (front+1) % 8;
+				System.out.println(Arrays.toString(pw));
+			}
+			
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < 8 ; i++) {
+			sb.append(pw[front]).append(" ");
+			front = (front+1) % 8;
+		}
+		System.out.println("#"+tc+" "+sb);
+	}
+}
+// cycle : 5 , 원소의 최소는 0이고 0이되면 종료 이시점의 값 = 암호
