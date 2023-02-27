6d432afbf06a76ae61c85b99250dc4b2eb850fdc
DHKIM-0511 <122420197+DHKIM-0511@users.noreply.github.com>
2023-02-28 00:59:28 +0900

    Boj solve

--- Algorithm/baekjoon/src/silver4/수찾기_1920.java
+++ Algorithm/baekjoon/src/silver4/수찾기_1920.java
@@ -3,13 +3,50 @@ package silver4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class 수찾기_1920 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
+		n = Integer.parseInt(br.readLine());
+		int[] arr= new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i< n ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		m = Integer.parseInt(br.readLine());
+		int[] search = new int[m];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < m ; i++) {
+			search[i] = Integer.parseInt(st.nextToken());
+		}
+		BS(arr,search);
+	}
+	static int n;
+	static int m;
+	private static void BS(int[] arr, int[] search) {
+		Arrays.sort(arr);
 		
-		
+		a:for(int i = 0 ; i < m ; i++) {
+			int start = 0;
+			int end = n-1;
+			
+			while(start <= end) {
+				int mid = (start+end)/2;
+				
+				if(arr[mid] == search[i]) {
+					System.out.println(1);
+					continue a;
+				}else if(arr[mid] > search[i]){
+					end = mid-1;
+				}else
+					start = mid+1;
+			}
+			System.out.println(0);
+		}
 		
 	}
 }
