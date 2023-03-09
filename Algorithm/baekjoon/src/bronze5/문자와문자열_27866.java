7b9d1a12241f438d7eff06dc7f4861daecd9048d
DHKIM-0511 <122420197+DHKIM-0511@users.noreply.github.com>
2023-03-09 23:56:26 +0900

    0309 study

---
+++ Algorithm/baekjoon/src/bronze5/문자와문자열_27866.java
@@ -0,0 +1,14 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class 문자와문자열_27866 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		int i = sc.nextInt();
+		
+		System.out.println(s.charAt(i-1));
+	}
+}
