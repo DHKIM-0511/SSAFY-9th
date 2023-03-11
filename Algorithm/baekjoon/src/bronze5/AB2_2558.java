516bddc61fc5247dcc70fe2609636337a61d1b63
DHKIM-0511 <122420197+DHKIM-0511@users.noreply.github.com>
2023-03-11 20:59:27 +0900

    0311study

---
+++ Algorithm/baekjoon/src/bronze5/AB2_2558.java
@@ -0,0 +1,14 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class AB2_2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+	}
+}
