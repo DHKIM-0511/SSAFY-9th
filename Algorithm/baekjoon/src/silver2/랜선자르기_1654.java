028441caebacbf31642bc709d55784ae8b5f0546
DHKIM-0511 <122420197+DHKIM-0511@users.noreply.github.com>
2023-03-12 21:56:04 +0900

    0312 study

--- Algorithm/baekjoon/src/silver2/랜선자르기_1654.java
+++ Algorithm/baekjoon/src/silver2/랜선자르기_1654.java
@@ -6,7 +6,7 @@ public class 랜선자르기_1654 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int k = sc.nextInt(); // 최초 랜선 수
-		int n = sc.nextInt(); // 이후 랜선 수
+		int n = sc.nextInt(); // 자른 후 랜선 수
 		
 		int[] line = new int[k]; // 각 랜선의 길이
 		int left = 0;
@@ -19,23 +19,22 @@ public class 랜선자르기_1654 {
 				right = line[i];
 			}
 		}
-		right++;
+		right++; // 초기값에서 0이 들어가는것을 방지
 		
-		while(left <= right) {
+		while(left < right) {
 			int mid = (left+right) /2;
-			//자를 길이
-			int sum = 0;
+			
 			//잘랐을 때 갯수
 			int cnt = 0;
 			
 			for(int i = 0 ; i < k ; i++) {
 				 cnt+=(line[i]/mid);
-				 if(cnt < n) {
-						right = mid;
-					}
-					else {
-						left = mid + 1;
-					}
+			}
+			if(cnt < n) {
+				right = mid;
+			}
+			else {
+				left = mid + 1;
 			}
 			
 		}
